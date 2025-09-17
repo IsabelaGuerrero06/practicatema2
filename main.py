8788a4a8b6b9cb6c9a53cdf9c866a96b6ca4687a
IsabelaGuerrero06 <guerreromahechai@gmail.com>
2025-09-17 17:24:11 -0500

    mezcla7

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 nombre="Jazmin"  
-<<<<<<< HEAD
 apellido="Duque" 
-=======
 apellido="Guerrero" 
->>>>>>> 6a71265854c78806359522434039f6bef3d5c9c5
 ombre="Isabela"   
 nombre="Alejandro"   
 nombre="Jazmin"   
